d13ca76d8d0293b678b2e152e06ffb68dd6397be
himanshu.ma <himanshu.ma@MacBook-Pro.local>
2022-12-10 23:24:37 +0530

    inital commit

---
+++ python/auth/server.py
@@ -0,0 +1,83 @@
+import jwt
+import datetime
+import os
+import os
+import datetime
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+# variables
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# config
+# server.config is dictionary here
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER") 
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+# print(server.config["MYSQL_HOST"])
+
+
+@server.route("/login", methods=["POST"])
+def login():
+    # get basic creds from authorization header : auth.username -> email / auth.password
+    auth = request.authorization
+    if not auth:
+        return "missing credientials", 401
+
+    # check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        f"SELECT email, password FROM user WHERE email={auth.username}")
+
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.username != password:
+            return "invalid creds", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+
+    else:
+        return "invalid creds", 401
+
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    print(encoded_jwt)
+    #Authorization:Bearer <token>
+
+    if not encoded_jwt:
+        return "missing credientials", 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1] # this will return the token
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+    return decoded, 200
+
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelata(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+
+if __name__ == "__main__":
+    server.run(port=5000, host="0.0.0.0")
---
+++ python/gateway/storage/util.py
@@ -0,0 +1,32 @@
+import pika
+import json
+
+
+def upload(f, fs, channel, access):
+    ''' 1. first the file is uploded to mongodb server
+        2. A msg is put in rabbitmq queue when file is uploaded successfully 
+        3. downstream service will pull up the msg, can process the file from monogodb 
+        4. queue create async flow between gateway service and processing service 
+    '''
+
+    try:
+        fid: fs.put(f)
+    except Exception as err:
+        return "internal server error", 500
+    message = {
+        "video_fid": str(fid),
+        "mp3_fid": None,
+        "username": access["username"]
+    }
+    try:
+        channel.basic_publish(
+            exchange="",
+            routing_key="video"  # name of queue,
+            body=json.dumps(message)
+            properties=pika.BasicProperites(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except:
+        fs.delete(fid)
+        return "internal server error", 500
